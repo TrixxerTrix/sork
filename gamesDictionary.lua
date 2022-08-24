3a262ff0f72753bc9e2e37ba962b83eab3b3c218
Trix <78498219+TrixxerTrix@users.noreply.github.com>
2022-08-24 14:30:18 +0100

    Update gamesDictionary.lua

--- gamesDictionary.lua
+++ gamesDictionary.lua
@@ -5,8 +5,7 @@ games[7363647365] = {
 	title = "sork (sbf...)",
 	stuff = {
 		{type = "string", callback = nil, name = "Message to say"},
-		{type = "button", callback = "speakMessage.lua", name = "Say"},
-		{type = "boolean", callback = "spamMessage.lua", name = "Spam"}
+		{type = "button", callback = "speakMessage.lua", name = "Say"}
 	}
 }
 
