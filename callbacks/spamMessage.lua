7b6f9d8cf1180f72d5e894b61d414d41f1444537
Trix <78498219+TrixxerTrix@users.noreply.github.com>
2022-08-24 14:29:51 +0100

    Delete spamMessage.lua

--- callbacks/spamMessage.lua
+++
@@ -1,9 +0,0 @@
-local event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
-
-if not game:GetService("Players").LocalPlayer:FindFirstChild("Sork"):GetAttribute("spamMessage_ran") then
-  game:GetService("Players").LocalPlayer:FindFirstChild("Sork"):SetAttribute("spamMessage_ran", true)
-  while game:GetService("Players").LocalPlayer:FindFirstChild("Sork"):IsDescendantOf(game:GetService("Players").LocalPlayer) and (getfenv().sork.global["Spam"]) do
-    event:FireServer(getfenv().sork.global["Message to say"], "All")
-    task.wait(1)
-  end
-end
