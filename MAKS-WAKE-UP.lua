208a5f4b5f6ff3e918b1ee8ec70c7d2d54336311
Trix <78498219+TrixxerTrix@users.noreply.github.com>
2022-09-29 11:22:53 +0100

    Update MAKS-WAKE-UP.lua

--- MAKS-WAKE-UP.lua
+++ MAKS-WAKE-UP.lua
@@ -1,6 +1,7 @@
 if not game:IsLoaded() then
 	game.Loaded:Wait()
 end
+game:GetService("ContentProvider"):PreloadAsync({"http://www.roblox.com/asset/?id=6021874488"})
 
 -- == VERY Useful Stuff == --
 
@@ -38,38 +39,38 @@ coroutine.resume(coroutine.create(function()
 	local gui = new("ScreenGui", core, {IgnoreGuiInset = true})
 	local blur = new("BlurEffect", workspace.CurrentCamera, {Size = 0, Enabled = true})
 	local overlay = new("Frame", gui, {BackgroundColor3 = Color3.new(), BackgroundTransparency = 1, Size = UDim2.new(1,0,1,0), BorderSizePixel = 0})
-	local icon = new("ImageLabel", overlay, {AnchorPoint = Vector2.new(.5,.5), BackgroundTransparency = 1, Image = "http://www.roblox.com/asset/?id=2565668394", ImageTransparency = 1, Size = UDim2.new(0.156, 0, 0.278, 0),ScaleType = Enum.ScaleType.Fit, Position = UDim2.new(0.5,0,0.5,0)})
+	local icon = new("ImageLabel", overlay, {AnchorPoint = Vector2.new(.5,.5), BackgroundTransparency = 1, Image = "http://www.roblox.com/asset/?id=6021874488", ImageTransparency = 1, Size = UDim2.new(0.156, 0, 0.278, 0),ScaleType = Enum.ScaleType.Fit, Position = UDim2.new(0.5,0,0.5,0)})
 	local title = new("TextLabel", overlay, {AnchorPoint = Vector2.new(.5,.5), BackgroundTransparency = 1, Font = Enum.Font.GothamMedium, Text = "trix_lib.exe", TextScaled = true, TextColor3 = Color3.new(1,1,1), Size = UDim2.new(0.163, 0, 0.056, 0), Position = UDim2.new(0.5, 0, 0.5, 0), TextTransparency = 1})
 	local subtitle = new("TextLabel", overlay, {AnchorPoint = Vector2.new(.5,.5), BackgroundTransparency = 1, Font = Enum.Font.Gotham, Text = quotes[Random.new():NextInteger(1, #quotes)], TextScaled = true, TextColor3 = Color3.new(1,1,1), Size = UDim2.new(0.181, 0, 0.026, 0), Position = UDim2.new(0.5, 0, 0.554, 0), TextTransparency = 1})
 	local icontween = tween:Create(icon, TweenInfo.new(2, Enum.EasingStyle.Quint, Enum.EasingDirection.InOut), {Rotation = 360})
 	local startedbool = false
 
-	tween:Create(overlay, TweenInfo.new(.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {BackgroundTransparency = .35}):Play()
-	tween:Create(blur, TweenInfo.new(.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {Size = 24}):Play()
+	tween:Create(overlay, TweenInfo.new(1, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {BackgroundTransparency = .35}):Play()
+	tween:Create(blur, TweenInfo.new(1, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {Size = 24}):Play()
 	started.Event:Once(function()
 		startedbool = true
 	end)
 
 	task.wait(1)
-	tween:Create(icon, TweenInfo.new(.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {ImageTransparency = 0}):Play()
+	tween:Create(icon, TweenInfo.new(1, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {ImageTransparency = 0}):Play()
 	repeat
 		icontween:Play()
 		icontween.Completed:Wait()
 		icon.Rotation = 0
 	until startedbool
 
-	local out = tween:Create(icon, TweenInfo.new(.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {ImageTransparency = 1})
+	local out = tween:Create(icon, TweenInfo.new(1, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {ImageTransparency = 1})
 	out:Play()
 	out.Completed:Wait() 
 
-	local titletween = tween:Create(title, TweenInfo.new(0.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {TextTransparency = 0})
+	local titletween = tween:Create(title, TweenInfo.new(1, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {TextTransparency = 0})
 	titletween:Play()
 	titletween.Completed:Wait()
 
 	task.wait(2)
 
-	local subtitletween = tween:Create(subtitle, TweenInfo.new(0.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {TextTransparency = 0.5, Position = UDim2.new(0.5, 0, 0.528, 0)})
-	local titletween_2 = tween:Create(title, TweenInfo.new(0.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {Position = UDim2.new(0.5, 0, 0.487, 0)})
+	local subtitletween = tween:Create(subtitle, TweenInfo.new(1, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {TextTransparency = 0.5, Position = UDim2.new(0.5, 0, 0.528, 0)})
+	local titletween_2 = tween:Create(title, TweenInfo.new(1, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {Position = UDim2.new(0.5, 0, 0.487, 0)})
 
 	subtitletween:Play()
 	titletween_2:Play()
@@ -77,13 +78,13 @@ coroutine.resume(coroutine.create(function()
 	subtitletween.Completed:Wait()
 	task.wait(3)
 
-	tween:Create(title, TweenInfo.new(0.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {TextTransparency = 1}):Play()
-	tween:Create(subtitle, TweenInfo.new(0.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {TextTransparency = 1}):Play()
+	tween:Create(title, TweenInfo.new(1, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {TextTransparency = 1}):Play()
+	tween:Create(subtitle, TweenInfo.new(1, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {TextTransparency = 1}):Play()
 	task.wait(1)
-	tween:Create(overlay, TweenInfo.new(.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {BackgroundTransparency = 1}):Play()
-	tween:Create(blur, TweenInfo.new(.5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {Size = 0}):Play()
-	debris:AddItem(blur, .5)
-	debris:AddItem(gui, .5)
+	tween:Create(overlay, TweenInfo.new(1, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {BackgroundTransparency = 1}):Play()
+	tween:Create(blur, TweenInfo.new(1, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {Size = 0}):Play()
+	debris:AddItem(blur, 1)
+	debris:AddItem(gui, 1)
 end))
 
 -- Instances --
@@ -97,7 +98,7 @@ end))
 -- == CORE == --
 
 -- Start --
-task.wait(1)
+task.wait(15)
 started:Fire()
 
 -- Connections --
